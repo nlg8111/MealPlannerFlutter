8ee0a023156fb2ebc564ce09d703eebeb2f3570e
Sami Kurvinen <sami.kurvinen@gmail.com>
2021-02-15 11:26:17 +0200

    Simple text fields for adding meal

--- lib/meal/new_meal_screen.dart
+++ lib/meal/new_meal_screen.dart
@@ -1,10 +1,63 @@
 import 'package:flutter/material.dart';
 
-class NewMealScreen extends StatelessWidget {
+class NewMealScreen extends StatefulWidget {
+  @override
+  _NewMealScreenState createState() => _NewMealScreenState();
+}
+
+class _NewMealScreenState extends State<NewMealScreen> {
+  String name;
+  String recipe;
+  String ingredients;
+
+  void addNewMeal() async {
+    print('Add meal:');
+    print(name);
+    print(recipe);
+    print(ingredients);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Text('New meal screen'),
+      child: Padding(
+        padding: const EdgeInsets.all(30.0),
+        child: ListView(
+          children: [
+            Text('New meal screen'),
+            TextField(
+              textAlign: TextAlign.left,
+              decoration: InputDecoration(labelText: 'Name'),
+              onChanged: (value) {
+                name = value;
+              },
+            ),
+            TextField(
+              textAlign: TextAlign.left,
+              decoration: InputDecoration(
+                labelText: 'Recipe',
+              ),
+              onChanged: (value) {
+                recipe = value;
+              },
+              minLines: null,
+              maxLines: null,
+            ),
+            TextField(
+              textAlign: TextAlign.left,
+              decoration: InputDecoration(
+                labelText: 'Ingredients',
+              ),
+              onChanged: (value) {
+                ingredients = value;
+              },
+              minLines: null,
+              maxLines: null,
+            ),
+            FlatButton(onPressed: addNewMeal, child: Text('Save'))
+          ],
+        ),
+      ),
     );
   }
 }
