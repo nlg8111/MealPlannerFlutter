72d60cf0d175be78128242b7b37c264444a20b90
Sami Kurvinen <sami.kurvinen@gmail.com>
2021-02-20 08:48:51 +0200

    Change routes to statics

--- lib/app_routes.dart
+++ lib/app_routes.dart
@@ -3,22 +3,12 @@ import 'package:meal_planner/meal/meal_route.dart';
 import 'package:meal_planner/meal/new_meal_route.dart';
 import 'package:meal_planner/welcome/welcome_route.dart';
 
-abstract class AppRoute {
-  String key;
-
-  Widget builder(BuildContext context);
-}
-
 class AppRoutes {
-  AppRoute welcome = WelcomeRoute();
-  AppRoute newMeal = NewMealRoute();
-  AppRoute meal = MealRoute();
-
-  String get initialRoute => welcome.key;
+  String get initialRoute => WelcomeRoute.key;
 
   Map<String, Widget Function(BuildContext)> get map => {
-        welcome.key: welcome.builder,
-        newMeal.key: newMeal.builder,
-        meal.key: meal.builder,
+        WelcomeRoute.key: WelcomeRoute.builder,
+        NewMealRoute.key: NewMealRoute.builder,
+        MealRoute.key: MealRoute.builder,
       };
 }
--- lib/welcome/welcome_route.dart
+++ lib/welcome/welcome_route.dart
@@ -1,10 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:meal_planner/app_routes.dart';
 import 'package:meal_planner/welcome/welcome_screen.dart';
 
-class WelcomeRoute implements AppRoute {
-  String key = '/welcome';
-  Widget builder(BuildContext context) {
+class WelcomeRoute {
+  static String key = '/welcome';
+  static Widget builder(BuildContext context) {
     return Scaffold(body: WelcomeScreen());
   }
 }
